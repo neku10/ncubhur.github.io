dd07976a82d99fde2f144cd5b601d59f55f069b3
neku10 <cubedo.nieves@gmail.com>
2023-03-05 22:28:21 +0100

    prueba camara

--- js/Proyecto.js
+++ js/Proyecto.js
@@ -31,11 +31,11 @@ function init()
     scene.background = new THREE.Color(0.5,0.5,0.5);
 
     // Instanciar la camara
-    camera= new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,50);
+    camera= new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,100);
     camera.position.set(0.5,2,3);
     cameraControls = new OrbitControls( camera, renderer.domElement );
     cameraControls.target.set(0,1,0);
-    camera.lookAt(0,1,0);
+    camera.lookAt(4,1,0);
 
     // Luces
     const ambiental = new THREE.AmbientLight(0x222222);
