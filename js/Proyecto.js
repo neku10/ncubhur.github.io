e4cf40a5b3038b2832e001427d87b62d91e75354
neku10 <cubedo.nieves@gmail.com>
2023-03-05 22:49:49 +0100

    cam

--- js/Proyecto.js
+++ js/Proyecto.js
@@ -34,8 +34,8 @@ function init()
     camera= new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,100);
     camera.position.set(0.5,2,3);
     cameraControls = new OrbitControls( camera, renderer.domElement );
-    cameraControls.target.set(-1,1,2);
-    camera.lookAt(-1,1,2);
+    cameraControls.target.set(5,1,-1);
+    camera.lookAt(5,1,-1);
 
     // Luces
     const ambiental = new THREE.AmbientLight(0x222222);
