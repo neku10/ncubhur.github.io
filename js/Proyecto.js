0629b09e9cf6cdfebd97ed2f3a5973c4b5728f51
neku10 <cubedo.nieves@gmail.com>
2023-03-05 22:18:15 +0100

    girar tablero

--- js/Proyecto.js
+++ js/Proyecto.js
@@ -101,7 +101,7 @@ function loadScene()
     glloader.load( 'models/tic_tac_toe/tablero.gltf', function ( gltf ) {
         gltf.scene.position.y = 1;
         gltf.scene.position.x = -1;
-        gltf.scene.rotation.y = 2*Math.PI;
+        gltf.scene.rotation.y = Math.PI/2;
         //esfera.add( gltf.scene );
         scene.add(gltf.scene);
     
@@ -138,6 +138,7 @@ function loadScene()
     glloader.load( 'models/wooden_bench_remastered/scene.gltf', function ( gltf ) {
         gltf.scene.position.y = 0;
         gltf.scene.position.x = -1;
+        gltf.scene.rotation.y = -Math.PI/2;
         //cubo.add( gltf.scene );
         scene.add(gltf.scene);
     
