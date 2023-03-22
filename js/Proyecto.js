fe5d8e59449830c89a83f2680af5611bc34089da
neku10 <cubedo.nieves@gmail.com>
2023-03-22 09:55:36 +0100

    Proyecto terminado

--- js/Proyecto.js
+++ js/Proyecto.js
@@ -4,32 +4,31 @@ import {GLTFLoader} from "../lib/GLTFLoader.module.js";
 import {OrbitControls} from "../lib/OrbitControls.module.js";
 import {TWEEN} from "../lib/tween.module.min.js";
 
+
 // Variables de consenso
 let renderer, scene, camera;
 
 // Otras globales
 let cameraControls, effectController;
-let mesa;
 let board;
 let piezas;
+let piezasTablero;
 let areasClicables;
 let estadoTablero;
 let jugador;
 let contador;
-let angulo = 0;
+let divResultado = document.getElementById("reinicio");
 
 // Acciones
 init();
 loadScene();
 render();
-
 //! Inicializacion del entorno: motor, camara y escena
 function init()
 {
     // Motor de render
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
-    //renderer.setClearColor( new THREE.Color(0x0000AA) ); //a que color pintar el fondo se puede hacer con scene.background
     document.getElementById('container').appendChild( renderer.domElement );
 
     // Escena
@@ -44,29 +43,29 @@ function init()
     camera.lookAt(0,1,0);
 
     // Luces
-    const ambiental = new THREE.AmbientLight(0x222222);
+    const ambiental = new THREE.AmbientLight(0x222222, 7);
     scene.add(ambiental);
     const direccional = new THREE.DirectionalLight(0xFFFFFF,0.3);
-    direccional.position.set(-1,1,-1);
+    direccional.position.set(-2,2,0);
     direccional.castShadow = true;
     scene.add(direccional);
-    const puntual = new THREE.PointLight(0xFFFFFF,0.5);
-    puntual.position.set(2,7,-4);
+    const puntual = new THREE.PointLight(0xFFFFFF,0.7);
+    puntual.position.set(2,6,1);
     scene.add(puntual);
-    const focal = new THREE.SpotLight(0xFFFFFF,0.3);
-    focal.position.set(-2,7,4);
-    focal.target.position.set(0,0,0);
+    const focal = new THREE.SpotLight(0xFFFFFF,0.7);
+    focal.position.set(-1.5,2,0.5);
+    focal.target.position.set(0,2,0);
     focal.angle= Math.PI/7;
     focal.penumbra = 0.3;
     focal.castShadow= true;
     focal.shadow.camera.far = 20;
     focal.shadow.camera.fov = 80;
     scene.add(focal);
-    scene.add(new THREE.CameraHelper(focal.shadow.camera));
 
     
     //renderer.domElement.addEventListener('dblclick', animate );
     renderer.domElement.addEventListener('dblclick', onClick, false);
+    divResultado.addEventListener('click',reiniciarJuego,false);
 
 }
 
@@ -76,8 +75,8 @@ function loadScene()
 {
     
     const path ="./images/";
-    const material = new THREE.MeshBasicMaterial({color: 'yellow', wireframe: true});
     board = new THREE.Object3D();
+    piezasTablero = new THREE.Object3D();
     let cruz1 = new THREE.Object3D();
     let cruz2 = new THREE.Object3D();
     let cruz3= new THREE.Object3D();
@@ -95,66 +94,7 @@ function loadScene()
     jugador = 'x';
     contador = 0;
 
-
-    //Area clicable
-    const geoCubo = new THREE.BoxGeometry( 0.15,0.03,0.15 );
-    areasClicables = new THREE.Group();
-
-        //PRIMERA COLUMNA
-    const cuboNE = new THREE.Mesh( geoCubo, material );
-    cuboNE.position.x = 0.175;
-    cuboNE.position.y = 0.05;
-    cuboNE.position.z = -0.175;
-    areasClicables.add(cuboNE);
-
-    const cuboE = new THREE.Mesh( geoCubo, material );
-    cuboE.position.y = 0.05;
-    cuboE.position.z = -0.175;
-    areasClicables.add(cuboE);
-
-    const cuboSE = new THREE.Mesh( geoCubo, material );
-    cuboSE.position.y = 0.05;
-    cuboSE.position.x = -0.175;
-    cuboSE.position.z = -0.175;
-    areasClicables.add(cuboSE);
-
-        //COLUMNA CENTRAL
-    const cuboN = new THREE.Mesh( geoCubo, material );
-    cuboN.position.x = 0.175;
-    cuboN.position.y = 0.05;
-    areasClicables.add(cuboN);
-
-    const cuboC = new THREE.Mesh( geoCubo, material );
-    cuboC.position.y = 0.05;
-    areasClicables.add(cuboC);
-
-    const cuboS = new THREE.Mesh( geoCubo, material );
-    cuboS.position.x = -0.175;
-    cuboS.position.y = 0.05;
-    areasClicables.add(cuboS);
-
-        //TERCERA COLUMNA
-    const cuboNO = new THREE.Mesh( geoCubo, material );
-    cuboNO.position.x = 0.175;
-    cuboNO.position.y = 0.05;
-    cuboNO.position.z = 0.175;
-    areasClicables.add(cuboNO);
-
-    const cuboO = new THREE.Mesh( geoCubo, material );
-    cuboO.position.y = 0.05;
-    cuboO.position.z = 0.175;
-    areasClicables.add(cuboO);
-
-    const cuboSO = new THREE.Mesh( geoCubo, material );
-    cuboSO.position.x = -0.175;
-    cuboSO.position.y = 0.05;
-    cuboSO.position.z = 0.175;
-    areasClicables.add(cuboSO);
-
-    board.add(areasClicables);
-    console.log('creaareas');
-
-
+    cargarAreasClicables();
 
     // Importar un modelo en gltf
     const glloader = new GLTFLoader();
@@ -164,9 +104,7 @@ function loadScene()
         //gltf.scene.position.x = -1;
         gltf.scene.rotation.y = Math.PI/2;
         gltf.scene.name = 'tablero';
-        board.add(gltf.scene)
-        //esfera.add( gltf.scene );
-        //scene.add(gltf.scene);
+        board.add(gltf.scene);
     
     }, undefined, function ( error ) {
     
@@ -178,7 +116,6 @@ function loadScene()
         gltf.scene.rotation.y = -Math.PI/2;
         //gltf.scene.position.x = 1;
         gltf.scene.name = 'cruz';
-        //cubo.add( gltf.scene );
         cruz1.add(gltf.scene);
         cruz2.add(gltf.scene.clone());
         cruz3.add(gltf.scene.clone());
@@ -196,13 +133,10 @@ function loadScene()
         //gltf.scene.position.y = 2;
         gltf.scene.rotation.y = -Math.PI/2;
         //gltf.scene.position.x = 1;
-        //cubo.add( gltf.scene );
-        //scene.add(gltf.scene);
         circulo1.add(gltf.scene);
         circulo2.add(gltf.scene.clone());
         circulo3.add(gltf.scene.clone());
         circulo4.add(gltf.scene.clone());
-
     
     }, undefined, function ( error ) {
     
@@ -214,7 +148,6 @@ function loadScene()
         gltf.scene.position.x = -1;
         gltf.scene.rotation.y = -Math.PI/2;
         gltf.scene.scale.set(1.5*gltf.scene.scale.x,1.5*gltf.scene.scale.y,1.5*gltf.scene.scale.z);
-        //mesa.add( gltf.scene );
         scene.add(gltf.scene);
     
     }, undefined, function ( error ) {
@@ -223,20 +156,10 @@ function loadScene()
     
     } );
 
-    // Objeto contenedor
-    //scene.add(mesa);
-    //board = new THREE.Object3D();
-    //board.position.y = 1.5;
-
     // Organizacion del grafo
     piezas = [cruz1, circulo1, cruz2, circulo2, cruz3, circulo3, cruz4, circulo4, cruz5];
+    board.add(piezasTablero);
     scene.add( board);
-
-    //board.add( cubo );
-    //board.add( esfera );
-    //cubo.add( new THREE.AxesHelper(1) );
-    //scene.add( new THREE.AxesHelper(3) );
-
 
     // Habitacion
     const paredes = [];
@@ -255,7 +178,67 @@ function loadScene()
     const habitacion = new THREE.Mesh( new THREE.BoxGeometry(40,40,40),paredes);
     scene.add(habitacion);
 
+}
 
+function cargarAreasClicables(){
+        //Area clicable
+        const geoCubo = new THREE.BoxGeometry( 0.15,0.03,0.15 );
+        const material = new THREE.MeshBasicMaterial({ transparent: true, wireframe: true, opacity: 0});
+        areasClicables = new THREE.Group();
+        areasClicables.name = "piezas";
+    
+            //PRIMERA COLUMNA
+        const cuboNO = new THREE.Mesh( geoCubo, material );
+        cuboNO.position.x = 0.175;
+        cuboNO.position.y = 0.05;
+        cuboNO.position.z = -0.175;
+        areasClicables.add(cuboNO);
+    
+        const cuboO = new THREE.Mesh( geoCubo, material );
+        cuboO.position.y = 0.05;
+        cuboO.position.z = -0.175;
+        areasClicables.add(cuboO);
+    
+        const cuboSO = new THREE.Mesh( geoCubo, material );
+        cuboSO.position.x = -0.175;
+        cuboSO.position.y = 0.05;
+        cuboSO.position.z = -0.175;
+        areasClicables.add(cuboSO);
+    
+            //COLUMNA CENTRAL
+        const cuboN = new THREE.Mesh( geoCubo, material );
+        cuboN.position.x = 0.175;
+        cuboN.position.y = 0.05;
+        areasClicables.add(cuboN);
+    
+        const cuboC = new THREE.Mesh( geoCubo, material );
+        cuboC.position.y = 0.05;
+        areasClicables.add(cuboC);
+    
+        const cuboS = new THREE.Mesh( geoCubo, material );
+        cuboS.position.x = -0.175;
+        cuboS.position.y = 0.05;
+        areasClicables.add(cuboS);
+    
+            //TERCERA COLUMNA
+        const cuboNE = new THREE.Mesh( geoCubo, material );
+        cuboNE.position.x = 0.175;
+        cuboNE.position.y = 0.05;
+        cuboNE.position.z = 0.175;
+        areasClicables.add(cuboNE);
+    
+        const cuboE = new THREE.Mesh( geoCubo, material );
+        cuboE.position.y = 0.05;
+        cuboE.position.z = 0.175;
+        areasClicables.add(cuboE);
+    
+        const cuboSE = new THREE.Mesh( geoCubo, material );
+        cuboSE.position.x = -0.175;
+        cuboSE.position.y = 0.05;
+        cuboSE.position.z = 0.175;
+        areasClicables.add(cuboSE);
+    
+        board.add(areasClicables);
 }
 function onClick(event){
     let x= event.clientX;
@@ -265,93 +248,121 @@ function onClick(event){
     const rayo = new THREE.Raycaster();
     rayo.setFromCamera(new THREE.Vector2(x,y), camera);
     let intersecciones = rayo.intersectObjects(areasClicables.children,true);
-    console.log('hola mundito');
-    console.log(intersecciones);
 
     if( intersecciones.length > 0 ){
+
         let index = areasClicables.children.findIndex((c) => c.uuid === intersecciones[0].object.uuid);
-        console.log(intersecciones[0].object.position);
         addPiece(intersecciones[0]);
+        let resultado = comprobarGanador();
+        if(resultado!=null){
+            divResultado.innerHTML = "EL GANADOR ES "+ jugador.toUpperCase() + " <br> " + "REINICIAR";
+            divResultado.style.display = "inline";
+            return;
+        }else if(contador==8){
+            divResultado.innerHTML = "EMPATE" + " <br> " + "REINICIAR";
+            divResultado.style.display = "inline";
+            return;
+        }
+        cambiarJugador();
         areasClicables.children.splice(index,1);
         contador++;
     }
 }
-//añade una pieza al tablero en el area clicada con doble click
-function addPiece(casilla){
-    console.log('jugador actual = ', jugador);
-    if (jugador == 'x'){
-        jugador = 'o';
+function actualizarEstadoTablero(casilla){
+    let index;
+    if(casilla.object.position.z==-0.175){   //primera columna
+        index = 0;
+    }else if(casilla.object.position.z==0){  //segunda columna
+        index = 1;
+    }else{                                   //tercera columna
+        index = 2;
+    }
+    if(casilla.object.position.x==0.175){    //fila arriba
+        estadoTablero[0][index]=jugador;
 
-    }else{
-        jugador = 'x';
+    }else if (casilla.object.position.x==0){ //medio
+        estadoTablero[1][index]=jugador;
+
+    }else{                                   //abajo
+        estadoTablero[2][index]=jugador;
 
     }
-    //movemos la pieza y la añadimos al tablero
-    piezas[contador].position.copy(casilla.object.position);
-    board.add(piezas[contador]);
 }
 
-function animate(event)
-{
-    // Capturar y normalizar
-    let x= event.clientX;
-    let y = event.clientY;
-    x = ( x / window.innerWidth ) * 2 - 1;
-    y = -( y / window.innerHeight ) * 2 + 1;
-    let intersectionPoint = new THREE.Vector3();
+function cambiarJugador(){
+    //cambiar el jugador actual
+    if (jugador == 'x'){
+        jugador = 'o';
+    }else{
+        jugador = 'x';
+    }
+}
+//Se comprueban las casillas del tablero para ver si el ultimo movimiento ha llevado al jugador a ganar la partida
+// si  el jugador ha ganado la partida, no puede continuar jugando por lo que se bloquean las casillas restantes
+function comprobarGanador(){ 
+    //comprobar filas 
+    if((estadoTablero[0][0]==estadoTablero[0][1] && estadoTablero[0][1]==estadoTablero[0][2] && estadoTablero[0][2]==jugador) ||
+        (estadoTablero[1][0]==estadoTablero[1][1] && estadoTablero[1][1]==estadoTablero[1][2] && estadoTablero[1][2]==jugador)||
+        (estadoTablero[2][0]==estadoTablero[2][1] && estadoTablero[2][1]==estadoTablero[2][2] && estadoTablero[2][2]==jugador)){
+        areasClicables = new THREE.Group();
+        areasClicables.name = "piezas";
+        return jugador;
+    }
+    //comprobar columnas
+    if((estadoTablero[0][0]==estadoTablero[1][0] && estadoTablero[1][0]==estadoTablero[2][0] && estadoTablero[2][0]==jugador) ||
+        (estadoTablero[0][1]==estadoTablero[1][1] && estadoTablero[1][1]==estadoTablero[2][1] && estadoTablero[2][1]==jugador)  ||
+        (estadoTablero[0][2]==estadoTablero[1][2] && estadoTablero[1][2]==estadoTablero[2][2] && estadoTablero[2][2]==jugador)){
+        areasClicables = new THREE.Group();
+        areasClicables.name = "piezas";
+        return jugador;
+    }
+    //comprobar diagonales
+    if((estadoTablero[0][0]==estadoTablero[1][1] && estadoTablero[1][1]==estadoTablero[2][2] && estadoTablero[2][2]==jugador) ||
+        (estadoTablero[0][2]==estadoTablero[1][1] && estadoTablero[1][1]==estadoTablero[2][0] && estadoTablero[2][0]==jugador)){
+        areasClicables = new THREE.Group();
+        areasClicables.name = "piezas";
+        return jugador;
+    }
+    return null;
+}
 
-    // Construir el rayo y detectar la interseccion
-    const rayo = new THREE.Raycaster();
-    rayo.setFromCamera(new THREE.Vector2(x,y), camera);
-    const tablero = scene.getObjectByName('tablero');
-    //const robot = scene.getObjectByName('robota');
-    let intersecciones = rayo.intersectObjects(tablero.children,true);
-    let planeNormal = new THREE.Vector3();
-    planeNormal.copy(camera.position).normalize();
-    let plane = new THREE.Plane();
-    plane.setFromNormalAndCoplanarPoint(planeNormal,scene.position);
-    //var vec = rayo.ray.intersectPlane(new THREE.Plane(new THREE.Vector3(0, 1, 0)), intersectionPoint);
-    rayo.ray.intersectPlane(plane, intersectionPoint);
-
-    
-
-    if( intersecciones.length > 0 ){
-        /*new TWEEN.Tween( tablero.position ).
-        to( {x:[0,0],y:[3,1],z:[0,0]}, 2000 ).
+//añade una pieza al tablero en el area clicada con doble click
+function addPiece(casilla){
+    actualizarEstadoTablero(casilla);
+    //movemos la pieza y la añadimos al tablero
+    piezas[contador].position.copy(casilla.object.position);
+    piezas[contador].position.y = 0.25;
+    piezasTablero.add(piezas[contador]);
+    const x = piezas[contador].position.x;
+    const z = piezas[contador].position.z;
+    //animamos la entrada de la pieza
+    new TWEEN.Tween( piezas[contador].position ).
+        to( {x:[x,x],y:[0.25,0.05],z:[z,z]}, 2000 ).
         interpolation( TWEEN.Interpolation.Bezier ).
         easing( TWEEN.Easing.Bounce.Out ).
         start();
-        */
-        //cruz.position.x=intersectionPoint.x;
-        //cruz.position.y=intersectionPoint.y;
-        //cruz.position.z=intersectionPoint.z;
-        
-        board.add( cruz);
-        cruz.position.copy(intersectionPoint);
-        
+    
+}
+function reiniciarJuego(){
+    //eliminamos las piezas del tablero
+    while (piezasTablero.children.length)
+    {
+        piezasTablero.remove(piezasTablero.children[0]);
     }
-/*
-    intersecciones = rayo.intersectObjects(robot.children,true);
-
-    if( intersecciones.length > 0 ){
-        new TWEEN.Tween( robot.rotation ).
-        to( {x:[0,0],y:[Math.PI,-Math.PI/2],z:[0,0]}, 5000 ).
-        interpolation( TWEEN.Interpolation.Linear ).
-        easing( TWEEN.Easing.Exponential.InOut ).
-        start();
-    }*/
-
-    
-    
-
+    while(areasClicables.children.length){
+        areasClicables.remove(areasClicables.children[0]);
+    }
+    //devolvemos al estado inicial
+    estadoTablero = [[1,1,1],[1,1,1],[1,1,1]];
+    jugador = 'x';
+    contador = 0;
+    divResultado.style.display = "none";
+    cargarAreasClicables();
 }
 
 //! Etapa de actualizacion para cada frame
 function update()
 {
-    angulo += 0.01;
-    //console.log(camera.position);
-    //board.rotation.y = angulo;   
     TWEEN.update();
 }
 
